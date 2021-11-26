2446d086d2c802974a4c19bd4c2c41f0d635d6be
Sandra <slopez@cifpfbmoll.eu>
2021-11-26 21:10:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/taskk6/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.taskk6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        showFoodOrder(getString(R.string.froyo_order_message));
+    }
+
+    public void showFoodOrder(String message) {
+        displayToast(message);
+        Intent intent = new Intent(this,Order.class);
+        startActivity(intent);
+    }
+}
